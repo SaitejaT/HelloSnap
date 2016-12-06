039d220f35bf8d6114e8ee9d5058c5bf992d1006
SaitejaT <saitejaturlapati@gmail.com>
2016-12-06 11:05:03 +0530

    Testing Snap CI

--- spec/integration/helloWorldSpec.js
+++ spec/integration/helloWorldSpec.js
@@ -17,7 +17,7 @@ describe("Hello World Server", function() {
     it("returns Hello World", function(done) {
     console.log("Heyy3");
       request.get(base_url, function(error, response, body) {
-        expect(body).toBe("Hello World");
+        expect(body).toBe("Hello Word");
         done();
       });
     });
