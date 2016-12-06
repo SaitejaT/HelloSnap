2e63d303abdc04569b592c2f211942e1031dff2c
SaitejaT <saitejaturlapati@gmail.com>
2016-12-06 11:20:17 +0530

    testing snap CI

--- spec/int/helloWorldSpec.js
+++ spec/int/helloWorldSpec.js
@@ -2,20 +2,17 @@ var request = require("request");
 
 var base_url = "http://localhost:8080/"
 
-console.log("He
 
 describe("Hello World Server", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
         expect(response.statusCode).toBe(200);
-      console.log(response.statusCode);
         done();
       });
     });
 
     it("returns Hello World", function(done) {
-    console.log("Heyy3");
       request.get(base_url, function(error, response, body) {
         expect(body).toBe("Hello Word");
         done();
